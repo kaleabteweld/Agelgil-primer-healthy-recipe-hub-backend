cc18f1c93d76afe18926db637d7f4b45e9dc72f0
kaleab Teweldbrhan Abay <42609047+kaleabteweld@users.noreply.github.com>
2024-10-04 20:55:09 +0300

    added NotificationRouter to app

--- src/Routes/Authorization.ts
+++ src/Routes/Authorization.ts
@@ -6,13 +6,14 @@ import { privateRecipeRouter, publicRecipeRouter } from "./Recipe";
 import { privateReviewRouter, publicReviewRouter } from "./Review";
 import { publicIngredientsRouter, privateIngredientsRouter } from "./Ingredient";
 import { publicMealPlannerRouter, privateMealPlannerRouter } from "./MealPlanner";
+import { publicNotificationRouter, privateNotificationRouter } from "./Notification";
 
 
 const publicRouter = express.Router();
 const privateRouter = express.Router();
 
-publicRouter.use([publicUserRouter, publicAuthenticationRouter, publicModeratorRouter, publicRecipeRouter, publicReviewRouter, publicIngredientsRouter, publicMealPlannerRouter]);
-privateRouter.use([privateUserRouter, privateAuthenticationRouter, privateModeratorRouter, privateRecipeRouter, privateReviewRouter, privateIngredientsRouter, privateMealPlannerRouter]);
+publicRouter.use([publicUserRouter, publicAuthenticationRouter, publicModeratorRouter, publicRecipeRouter, publicReviewRouter, publicIngredientsRouter, publicMealPlannerRouter, publicNotificationRouter]);
+privateRouter.use([privateUserRouter, privateAuthenticationRouter, privateModeratorRouter, privateRecipeRouter, privateReviewRouter, privateIngredientsRouter, privateMealPlannerRouter, privateNotificationRouter]);
 
 
 export { publicRouter, privateRouter }
